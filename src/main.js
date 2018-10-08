d2cb389a4073452b543027da718aa9751887cd1f
yangkuo1 <yangkuo1@100tal.com>
2018-10-08 10:56:43 +0800

    vw

--- src/main.js
+++ src/main.js
@@ -6,12 +6,16 @@ import FastClick from 'fastclick'
 import axios from '@/api/api.js'
 import '@/filters/'
 import '@/directives'
+import 'normalize.css'
 Vue.config.productionTip = false
 // FastClick
 if ('addEventListener' in document) {
   document.addEventListener('DOMContentLoaded', function () {
     FastClick.attach(document.body)
   }, false)
+}
+window.onload = function () {
+  window.viewportUnitsBuggyfill.init({ hacks: window.viewportUnitsBuggyfillHacks })
 }
 Vue.prototype.$http = axios
 new Vue({
